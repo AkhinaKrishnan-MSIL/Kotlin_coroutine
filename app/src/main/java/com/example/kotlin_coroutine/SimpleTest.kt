868eadc70a3ab778cb41a9f43d6c0d5a225237c4
Akhina Krishnan <akhina.krishnan@inxsasia.com>
2022-04-25 21:42:59 +0530

    kotlin coroutine runblock

---
+++ app/src/main/java/com/example/kotlin_coroutine/SimpleTest.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_coroutine
+
+import org.junit.Assert
+import org.junit.Test
+
+class SimpleTest {
+    @Test
+    fun myfirsttest() {
+ Assert.assertEquals(20,10+10)
+    }
+}
--- app/src/main/java/com/example/kotlin_coroutine/coroutine_runblock.kt
+++ app/src/main/java/com/example/kotlin_coroutine/coroutine_runblock.kt
@@ -5,12 +5,14 @@ fun main()= runBlocking {
     println("main program starts: ${Thread.currentThread().name}")
 
     val jobDeferred: Deferred<String> =async{
-       println("fake work starts: ${Thread.currentThread().name}")
-      delay(1500)
+        println("fake work starts: ${Thread.currentThread().name}")
+        delay(1500)
         println("fake work ends: ${Thread.currentThread().name}")
         "Akhina Krishnan"
     }
-    val num : String =jobDeferred.await()
-    //jobDeferred.join()
     println("main program ends: ${Thread.currentThread().name}")
+}
+suspend fun myownsuspendfunction()
+{
+    delay(1000)
 }
